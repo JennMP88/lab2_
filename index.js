7ef3275f46d9fe46f4c4956962adff5b6f886de0
Jennifer Perez <jenniferperez@pursuit.org>
2019-01-04 21:21:38 -0500

    some changes were done

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const express = require('express');
 const app = express ();
-const port= 8000;
+const port= 8080;
+const file=require(`./routes/readwrite.js`)
+//check if class exist
+//-------------------TO BE DONE 
+//if exist then update {load, process data(json.parse), 
+//check if student exist {rewrite student data}}
+//response.send should look like the ask 
+//added:req.queryname....etc.
+//class:req.query.class
+
+app.get("/class/add",(req,res)=>{
+    let currentData;
+    file.load(req.query.name,(data,classname)=>{
+        currentData=JSON.parse(data);
+        res.send({
+            message:currentData,
+        })
+    })
+    
+})
+
 
 app.listen(port,()=>{
     console.log('listening to port')
 })
+
+// const Me={
+//     name:"jen",
+//     age:22,
+// }
+
+// const lol = JSON.stringify(Me);
+// file.save(`Name`, lol, (err,response)=>{
+//     console.log("hi")
+
+// })
+
+///class/add/?class=physics&name=John&age=30&city=NYC&grade=75
+//request.query.name for name...etc 
+//request.query.age for name
+//request.query.city for name
+//request.query.grade 
+
--- routes/readwrite.js
+++ routes/readwrite.js
@@ -1,1 +1,37 @@
 //read and writes file 
+
+
+
+const fs = require('fs');
+
+const load = (classname, cb) => {
+    console.log("Loading");
+    fs.readFile(`./classes/${classname}.json`, (err, data) => {
+        if (err) {
+            console.log('error');
+            return "No file found"
+        }
+        cb(data);
+    })
+}
+
+const save = (classname, data,cb) => {
+    console.log("saving")
+    fs.writeFile(`./classes/${classname}.json`, data, (err,res)=> {
+        console.log(err,res);
+        
+        cb(data)
+    })
+}
+
+// const add=(className,fileData,cb)=>{
+//     load(className,(data)=>{
+
+//         // cb(data);
+//     })
+// }
+
+module.exports={
+    load,
+    save,
+}
